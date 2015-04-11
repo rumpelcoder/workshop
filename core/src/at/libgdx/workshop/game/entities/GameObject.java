291323797e7d34142cb53c1282a354f9c822d8f9
Lukas Knoch <l.knoch@gmx.at>
2015-04-11 15:35:26 +0200

    added the abstract GameObject class

---
+++ core/src/at/libgdx/workshop/game/entities/GameObject.java
@@ -0,0 +1,32 @@
+package at.libgdx.workshop.game.entities;
+
+import at.libgdx.workshop.utils.Assets;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Lukas on 11.04.2015.
+ */
+public abstract class GameObject {
+    public Vector2 position;
+    public Vector2 dimension;
+    public Vector2 origin;
+    public Vector2 scale;
+    public float rotation;
+    protected Assets assets;
+
+    public GameObject() {
+        position = new Vector2();
+        dimension = new Vector2(1, 1);
+        origin = new Vector2();
+        rotation = 0;
+        assets = Assets.getInstance(new AssetManager());
+    }
+
+    public void update(float deltaTime) {
+    }
+
+    public abstract void render(SpriteBatch batch);
+}
+}
