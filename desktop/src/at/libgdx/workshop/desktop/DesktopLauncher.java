9c9c9d5796206b85abb23518f8ec60078a9b8226
Lukas Knoch <l.knoch@gmx.at>
2015-04-11 15:24:47 +0200

    added texture packer

--- desktop/src/at/libgdx/workshop/desktop/DesktopLauncher.java
+++ desktop/src/at/libgdx/workshop/desktop/DesktopLauncher.java
@@ -1,12 +1,24 @@
 package at.libgdx.workshop.desktop;
 
+import at.libgdx.workshop.JumpAndRoll;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import at.libgdx.workshop.JumpAndRoll;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    private static boolean rebuildAtlas = true;
+
+    public static void main(String[] arg) {
+        if (rebuildAtlas) {
+            TexturePacker.Settings settings = new TexturePacker.Settings();
+            settings.edgePadding = true;
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.debug = false;
+            settings.duplicatePadding = true; //avoid inner padding
+            TexturePacker.process(settings, "../../assets_raw", "images", "assets");
+        }
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new JumpAndRoll(), config);
 	}
 }
