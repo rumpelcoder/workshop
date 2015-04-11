e98d56f225d6849a0f2ccbca487a3b031da05e83
Lukas Knoch <l.knoch@gmx.at>
2015-04-11 12:47:05 +0200

    initial import

---
+++ android/src/at/libgdx/workshop/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package at.libgdx.workshop.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import at.libgdx.workshop.JumpAndRoll;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpAndRoll(), config);
+	}
+}
---
+++ desktop/src/at/libgdx/workshop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.libgdx.workshop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.libgdx.workshop.JumpAndRoll;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpAndRoll(), config);
+	}
+}
