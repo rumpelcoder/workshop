9fc0c824348f8e35395acf302a3d1505b36499b0
Lukas Knoch <l.knoch@gmx.at>
2015-04-11 15:14:51 +0200

    added input listener to control CameraHelper

--- core/src/at/libgdx/workshop/game/WorldController.java
+++ core/src/at/libgdx/workshop/game/WorldController.java
@@ -1,11 +1,14 @@
 package at.libgdx.workshop.game;
 
 import at.libgdx.workshop.utils.CameraHelper;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 
 /**
  * Created by Lukas on 11.04.2015.
  */
-public class WorldController {
+public class WorldController extends InputAdapter {
     public CameraHelper cameraHelper;
 
     public WorldController() {
@@ -13,10 +16,24 @@ public class WorldController {
     }
 
     public void init() {
+        Gdx.input.setInputProcessor(this);
         cameraHelper = new CameraHelper();
     }
 
     public void update(float deltaTime) {
         cameraHelper.update(deltaTime);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.PLUS:
+                cameraHelper.addZoom(-0.2f);
+                break;
+            case Input.Keys.MINUS:
+                cameraHelper.addZoom(0.2f);
+                break;
+        }
+        return true;
     }
 }
